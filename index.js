21e590b14eb581d215518068aa7b43b6c77b7691
Joseph Crump <josephcrump79@yahoo.com>
2024-11-12 13:55:29 -0600

    phase-1-class-extension-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((acc, side) => acc + side, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides !== 3) return false;
+
+        const [a, b, c] = this.sides;
+        return this.sides.every((side)=> side === this.sides[0]);
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides !== 4) return false;
+        return this.sides.every((side)=> side === this.sides[0]);
+    }
+    get area() {
+        if (this.isValid) {
+          return this.sides[0] ** 2;
+        }
+        return null;
+      }
+}
+
